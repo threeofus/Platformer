cb974787b63f90408463f09cc3fbbc58819303f5
Adam Jensen <oojensen@stanford.edu>
2012-07-31 23:37:44 -0500

    set up git ignore

---
+++ src/com/threeofus/Platformer.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package com.threeofus;
+
+import org.newdawn.slick.Game;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+/**
+ * @author adam
+ *
+ */
+public class Platformer implements Game {
+
+	/**
+	 * 
+	 */
+	public Platformer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.newdawn.slick.Game#closeRequested()
+	 */
+	@Override
+	public boolean closeRequested() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.newdawn.slick.Game#getTitle()
+	 */
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.newdawn.slick.Game#init(org.newdawn.slick.GameContainer)
+	 */
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.newdawn.slick.Game#render(org.newdawn.slick.GameContainer, org.newdawn.slick.Graphics)
+	 */
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.newdawn.slick.Game#update(org.newdawn.slick.GameContainer, int)
+	 */
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
